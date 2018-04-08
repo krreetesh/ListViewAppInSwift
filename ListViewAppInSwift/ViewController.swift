3e8de21af382f4586fbb9dff16c2a64ed7d104fb
Reetesh Kumar <reeteshkumar@shanmugams-macbook-pro-4.local>
2018-04-08 22:39:05 +0530

    Project setup in Swift without xib & storyboard

---
+++ ListViewAppInSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ListViewAppInSwift
+//
+//  Created by Reetesh Kumar on 08/04/18.
+//  Copyright © 2018 Reetesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
