5387636f1be7cf3560d849ddee75314fa004016e
Reetesh Kumar <reeteshkumar@shanmugams-macbook-pro-4.local>
2018-04-14 00:27:10 +0530

    Added TableView

--- ListViewAppInSwift/ViewController.swift
+++ ListViewAppInSwift/ViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let myArray: NSArray = ["First","Second","Third"]
+    private var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.cyan
+        
+        myTableView = UITableView(frame: self.view.bounds, style: .grouped)
+        
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +31,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(myArray[indexPath.row])")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+    
 
 }
 
